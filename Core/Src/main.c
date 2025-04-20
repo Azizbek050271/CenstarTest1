8e9a07ba53acde690a4e5be0f7c0dce796bd233f
Azizbek050271 <azerhud@gmail.com>
2025-04-20 15:17:06 +0500

    restoreAll

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -27,17 +27,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
-<<<<<<< HEAD
-=======
-#include "ssd1306.h"
-#include "ssd1306_fonts.h"
-#include "usart.h"
->>>>>>> parent of a704879 (update EEPROM)
 
-I2C_HandleTypeDef hi2c1;
-UART_HandleTypeDef huart2;
-UART_HandleTypeDef huart3;
+void SystemClock_Config(void);
 
+char display_buffer[40] = "Press a key...";
+char eeprom_data[40] = "";
 
 GPIO_TypeDef* row_ports[5] = { GPIOC, GPIOC, GPIOC, GPIOC, GPIOC };
 uint16_t      row_pins[5]  = { GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3, GPIO_PIN_4 };
@@ -52,38 +46,21 @@ char keymap[5][4] = {
     { 'E', '*', '0', '<' }
 };
 
-char display_buffer[40] = "Press a key...";
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-
-int main(void)
-{
-    HAL_Init();
-    SystemClock_Config();
-    MX_GPIO_Init();
-    MX_I2C1_Init();
-    MX_USART2_UART_Init();
-    // MX_USART3_UART_Init(); // Временно закомментировано для отладки
-
-    ssd1306_Init();
-    Update_Display("Hello!");
-    HAL_Delay(2000);
-    Update_Display(display_buffer);
-
-    while (1)
-    {
-        HAL_Delay(50);
-    }
-}
-
-<<<<<<< HEAD
-=======
 void Update_Display(const char* text)
 {
     ssd1306_Fill(Black);
     ssd1306_SetCursor(0, 0);
     ssd1306_WriteString((char*)text, Font_7x10, White);
+    ssd1306_UpdateScreen();
+}
+
+void Update_Display_2Lines(const char* line1, const char* line2)
+{
+    ssd1306_Fill(Black);
+    ssd1306_SetCursor(0, 0);
+    ssd1306_WriteString((char*)line1, Font_7x10, White);
+    ssd1306_SetCursor(0, 12);
+    ssd1306_WriteString((char*)line2, Font_7x10, White);
     ssd1306_UpdateScreen();
 }
 
@@ -99,6 +76,7 @@ void log_printf(const char *fmt, ...)
 
 #define DEBOUNCE_DELAY 50
 #define HOLD_CLEAR_DELAY 1000
+#define EEPROM_ADDR 0x00
 
 void Scan_Keyboard(void)
 {
@@ -109,129 +87,102 @@ void Scan_Keyboard(void)
     char current_key = 0;
     uint8_t key_detected = 0;
 
-    for (uint8_t row = 0; row < 5; row++)
-    {
+    for (uint8_t row = 0; row < 5; row++) {
         for (uint8_t r = 0; r < 5; r++)
             HAL_GPIO_WritePin(row_ports[r], row_pins[r], (r == row) ? GPIO_PIN_RESET : GPIO_PIN_SET);
 
         HAL_Delay(1);
 
-        for (uint8_t col = 0; col < 4; col++)
-        {
-            if (HAL_GPIO_ReadPin(col_ports[col], col_pins[col]) == GPIO_PIN_RESET)
-            {
+        for (uint8_t col = 0; col < 4; col++) {
+            if (HAL_GPIO_ReadPin(col_ports[col], col_pins[col]) == GPIO_PIN_RESET) {
                 current_key = keymap[row][col];
                 key_detected = 1;
                 break;
             }
         }
-
         if (key_detected) break;
     }
 
     uint32_t now = HAL_GetTick();
 
-    if (key_detected)
-    {
-        if (!key_is_pressed && (now - last_press_time > DEBOUNCE_DELAY))
-        {
+    if (key_detected) {
+        if (!key_is_pressed && (now - last_press_time > DEBOUNCE_DELAY)) {
             key_is_pressed = 1;
             last_press_time = now;
             key_hold_start_time = now;
 
             log_printf("Key detected: %c\r\n", current_key);
 
-            if (current_key == 'C')
-            {
+            if (current_key == 'C') {
                 strcpy(display_buffer, "Press a key...");
+                Update_Display(display_buffer);
                 log_printf("Action: Clear screen\r\n");
-            }
-            else if (current_key == '<')
-            {
+
+            } else if (current_key == '<') {
                 size_t len = strlen(display_buffer);
-                if (len > 0 && strcmp(display_buffer, "Press a key...") != 0)
-                {
+                if (len > 0 && strcmp(display_buffer, "Press a key...") != 0) {
                     display_buffer[len - 1] = '\0';
+                    Update_Display(display_buffer);
                     log_printf("Action: Delete last char\r\n");
                 }
-            }
-            else
-            {
+
+            } else if (current_key == 'G') {
+                HAL_StatusTypeDef res = EEPROM_Write(EEPROM_ADDR, (uint8_t*)display_buffer, strlen(display_buffer)+1);
+                log_printf("Action: Write EEPROM (%d)\r\n", res);
+                Update_Display("Write EEPROM");
+                HAL_Delay(1000);
+                Update_Display(display_buffer);
+
+            } else if (current_key == 'H') {
+                memset(eeprom_data, 0, sizeof(eeprom_data));
+                HAL_StatusTypeDef res = EEPROM_Read(EEPROM_ADDR, (uint8_t*)eeprom_data, sizeof(eeprom_data));
+                log_printf("Action: Read EEPROM (%d)\r\n", res);
+                Update_Display_2Lines("Read EEPROM", eeprom_data);
+                HAL_Delay(1000);
+                Update_Display(display_buffer);
+
+            } else {
                 if (strcmp(display_buffer, "Press a key...") == 0)
                     display_buffer[0] = '\0';
 
-                if (strlen(display_buffer) < sizeof(display_buffer) - 1)
-                {
+                if (strlen(display_buffer) < sizeof(display_buffer) - 1) {
                     size_t len = strlen(display_buffer);
                     display_buffer[len] = current_key;
                     display_buffer[len + 1] = '\0';
+                    Update_Display(display_buffer);
                     log_printf("Action: Append char '%c'\r\n", current_key);
                 }
             }
-        }
-        else if (key_is_pressed && current_key == 'C' && (now - key_hold_start_time > HOLD_CLEAR_DELAY))
-        {
+        } else if (key_is_pressed && current_key == 'C' && (now - key_hold_start_time > HOLD_CLEAR_DELAY)) {
             strcpy(display_buffer, "Press a key...");
+            Update_Display(display_buffer);
             key_hold_start_time = now + 10000;
             log_printf("Action: Hold-clear triggered\r\n");
         }
-    }
-    else if (key_is_pressed && (now - last_press_time > DEBOUNCE_DELAY))
-    {
+    } else if (key_is_pressed && (now - last_press_time > DEBOUNCE_DELAY)) {
         key_is_pressed = 0;
     }
 }
 
-static void MX_I2C1_Init(void)
+int main(void)
 {
-    hi2c1.Instance = I2C1;
-    hi2c1.Init.ClockSpeed = 400000;
-    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_16_9;
-    hi2c1.Init.OwnAddress1 = 0;
-    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-    hi2c1.Init.OwnAddress2 = 0;
-    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-    if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-    {
-        Error_Handler();
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+    MX_I2C1_Init();
+    MX_USART2_UART_Init();
+
+    ssd1306_Init();
+    Update_Display("Hello!");
+    HAL_Delay(2000);
+    Update_Display(display_buffer);
+
+    while (1) {
+        Scan_Keyboard();
+        HAL_Delay(50);
     }
 }
 
-static void MX_GPIO_Init(void)
-{
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOH_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    __HAL_RCC_GPIOD_CLK_ENABLE();
-    __HAL_RCC_GPIOE_CLK_ENABLE();
-
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_11|GPIO_PIN_12;
-    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
-}
-
->>>>>>> parent of a704879 (update EEPROM)
 void SystemClock_Config(void)
 {
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -248,20 +199,18 @@ void SystemClock_Config(void)
     RCC_OscInitStruct.PLL.PLLN = 336;
     RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
     RCC_OscInitStruct.PLL.PLLQ = 7;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    {
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
         Error_Handler();
     }
 
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                                  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-    {
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
         Error_Handler();
     }
 }
@@ -275,6 +224,6 @@ void Error_Handler(void)
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t *file, uint32_t line)
 {
-    // Optional: printf("Wrong parameters value: file %s on line %d\r\n", file, line);
+    // Optional
 }
 #endif
